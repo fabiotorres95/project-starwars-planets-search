b3e21ce26417d9ec8f2fbf539bf12dd307da4017
fabiotorres95 <engfabiotorres@outlook.com>
2023-03-28 17:15:17 -0300

    created name filter

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,47 +1,58 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
-function Table(props) {
-  const { list } = props;
+function Table({ list }) {
+  const [nameFilter, setNameFilter] = useState('');
+
+  const filteredList = list.filter((obj) => obj.name.includes(nameFilter));
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Rotation Period</th>
-          <th>Orbital Period</th>
-          <th>Diameter</th>
-          <th>Climate</th>
-          <th>Gravity</th>
-          <th>Terrain</th>
-          <th>Surface Water</th>
-          <th>Population</th>
-          <th>Films</th>
-          <th>Created</th>
-          <th>Edited</th>
-          <th>URL</th>
-        </tr>
-      </thead>
-      <tbody>
-        {list.map((obj) => (
-          <tr key={ obj.name }>
-            <td>{obj.name}</td>
-            <td>{obj.rotation_period}</td>
-            <td>{obj.orbital_period}</td>
-            <td>{obj.diameter}</td>
-            <td>{obj.climate}</td>
-            <td>{obj.gravity}</td>
-            <td>{obj.terrain}</td>
-            <td>{obj.surface_water}</td>
-            <td>{obj.population}</td>
-            <td>{obj.films}</td>
-            <td>{obj.created}</td>
-            <td>{obj.edited}</td>
-            <td>{obj.url}</td>
+    <>
+      <input
+        data-testid="name-filter"
+        placeholder="name"
+        value={ nameFilter }
+        onChange={ ({ target }) => setNameFilter(target.value) }
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface Water</th>
+            <th>Population</th>
+            <th>Films</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>URL</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {filteredList.map((obj) => (
+            <tr key={ obj.name }>
+              <td>{obj.name}</td>
+              <td>{obj.rotation_period}</td>
+              <td>{obj.orbital_period}</td>
+              <td>{obj.diameter}</td>
+              <td>{obj.climate}</td>
+              <td>{obj.gravity}</td>
+              <td>{obj.terrain}</td>
+              <td>{obj.surface_water}</td>
+              <td>{obj.population}</td>
+              <td>{obj.films}</td>
+              <td>{obj.created}</td>
+              <td>{obj.edited}</td>
+              <td>{obj.url}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
 
